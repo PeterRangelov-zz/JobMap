d9b942dc62ec25ee95772e435821d0f52d032e51
Peter Rangelov <peter.rangelov11@gmail.com>
2015-06-30 22:56:29 -0400

    testing env vs prop

--- app/util/Env.java
+++ app/util/Env.java
@@ -9,4 +9,8 @@ public class Env {
     public static String get (String key) {
         return Play.application().configuration().getString(key);
     }
+
+    public static final String MAILCHIMP_API_KEY = "mailchimp.apikey";
+    public static final String MAILCHIMP_LIST_ID = "mailchimp.listid";
+
 }
